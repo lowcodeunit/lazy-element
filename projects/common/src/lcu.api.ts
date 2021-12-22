045d1eca0906d28897ded001a9ffcc31ba0190bf
xchanin <shannonwbruns@gmail.com>
2021-12-22 14:06:13 -0700

    export lazy elements

--- projects/common/src/lcu.api.ts
+++ projects/common/src/lcu.api.ts
@@ -1,4 +1,5 @@
 export * from './lib/controls/lazy-element/lazy-element.component';
+export * from './lib/controls/lazy-elements/lazy-elements.component';
 export * from './lib/core/lazy-element-config';
 export * from './lib/pipes/push-pipe';
 export * from './lib/lazy-element.module';
